57b3fb32e2bb5f3561d206a40f6765af0e2a7937
douglas5001 <douglasportella66@gmail.com>
2024-04-16 21:41:11 -0300

    Preparando Pastas3

---
+++ static/scripts/javaScript.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.carousel-image');
+let currentSlide = 0;
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(nextSlide, 5000);
